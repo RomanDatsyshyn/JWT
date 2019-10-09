28770abe04e067e6592f69a8ae453725deb4397c
Roman Datsyshyn <39221029+RomanDatsyshyn@users.noreply.github.com>
2019-10-09 03:19:51 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoDB = "mongodb://drv:hashflare28@ds345937.mlab.com:45937/summer";
+const mongoDB = "mongodb://";
 
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
